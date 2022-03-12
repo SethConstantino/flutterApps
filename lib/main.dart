a1bc04a6c276f1df41c80bfed7681ca0ef797e2c
Seth Constantino <jaircm.ti20@utsjr.com>
2022-03-12 15:40:43 -0600

    repositorioNuevo

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:test_flutter/screens/datos.dart';
+import 'package:test_flutter/screens/paginaDos.dart';
+import 'package:test_flutter/screens/paginaTres.dart';
+import 'package:test_flutter/screens/TestStack.dart';
+import 'package:test_flutter/screens/paginaForm.dart';
+
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/segundo': (context) => const PaginaDos(),
+        '/tercero': (context) => const PaginaTres(),
+        '/datos': (context) => const Datos(),
+        '/testSatck': (context) => const TestStack(),
+        '/formulario': (context) => const PaginaForm()
+      }
+      //home: HomePage(),
+      ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        backgroundColor: Color.fromARGB(255, 219, 21, 21),
+        centerTitle: true,
+        title: const Text('Pagina inicial'),
+        elevation: 20.0,
+        shadowColor: Color.fromARGB(255, 192, 167, 167),
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(15))),
+        actions: <Widget>[
+          IconButton(icon: const Icon(Icons.search), onPressed: () => {})
+        ],
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("assets/img/jocho.jpg", width: 370.0),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        Color.fromARGB(255, 170, 128, 128)),
+                    padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(
+                            horizontal: 25, vertical: 15)),
+                    textStyle: MaterialStateProperty.all(
+                        const TextStyle(fontSize: 15))),
+                child: const Text('Segunda Pantalla'),
+                onPressed: () {
+                  final route = MaterialPageRoute(
+                    builder: (context) => const PaginaDos(),
+                  );
+                  Navigator.pushNamed(context, '/segundo');
+                }),
+            const SizedBox(height: 10.0),
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        Color.fromARGB(255, 170, 128, 128)),
+                    padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(
+                            horizontal: 25, vertical: 15)),
+                    textStyle: MaterialStateProperty.all(
+                        const TextStyle(fontSize: 15))),
+                child: const Text('Tercera Pantalla'),
+                onPressed: () {
+                  final route = MaterialPageRoute(
+                    builder: (context) => const PaginaTres(),
+                  );
+                  Navigator.pushNamed(context, '/tercero');
+                }),
+            const SizedBox(height: 10.0),
+            ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        Color.fromARGB(255, 170, 128, 128)),
+                    padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(
+                            horizontal: 25, vertical: 15)),
+                    textStyle: MaterialStateProperty.all(
+                        const TextStyle(fontSize: 15))),
+                child: const Text('Pantalla Datos'),
+                onPressed: () {
+                  final route = MaterialPageRoute(
+                    builder: (context) => const TestStack(),
+                  );
+                  Navigator.pushNamed(context, '/testSatck');
+                }),
+            const SizedBox(height: 10.0),
+            ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 170, 128, 128)),
+                  padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(horizontal: 25, vertical: 15)),
+                ),
+                child: const Text('Datos'),
+                onPressed: () {
+                  final route = MaterialPageRoute(
+                    builder: (context) => const Datos(),
+                  );
+                  Navigator.pushNamed(context, '/datos');
+                }),
+            const SizedBox(height: 10.0),
+            ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 170, 128, 128)),
+                  padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(horizontal: 25, vertical: 15)),
+                ),
+                child: const Text('Datos Contar'),
+                onPressed: () {
+                  final route = MaterialPageRoute(
+                    builder: (context) => const Datos(),
+                  );
+                  Navigator.pushNamed(context, '/formulario');
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/TestStack.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class TestStack extends StatelessWidget {
+  const TestStack({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Página"),
+        backgroundColor: Color.fromARGB(255, 134, 143, 150),
+      ),
+      //body: stackSinPos()
+      body: Center(
+        child: SizedBox(
+          width: 300.0,
+          height: 300.0,
+          child: Center(
+            child: Stack(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              fit: StackFit.expand,
+              overflow: Overflow.visible,
+              children: <Widget>[
+                ////IMAGEN/////////
+                Container(
+                  width: 300.0,
+                  height: 300.0,
+                  //color: Colors.red,
+                  padding: const EdgeInsets.all(15.0),
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/img/yo.jpg'))),
+
+                  //TEXTO//
+                ),
+                Positioned(
+                    top: 200.0,
+                    left: 70.0,
+                    child: Container(
+                      width: 180.0,
+                      height: 20.0,
+                      //color: Colors.black,
+                      child: const Text('Seth Jair Arturo Constantino',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 255, 255))),
+                    )),
+
+                //BOTON
+                Positioned(
+                    top: 230.0,
+                    left: 81.0,
+                    child: Container(
+                      width: 150.0,
+                      height: 50.0,
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                      Color.fromARGB(255, 2, 96, 119)),
+                                  padding: MaterialStateProperty.all(
+                                      const EdgeInsets.symmetric(
+                                          horizontal: 25, vertical: 15)),
+                                  textStyle: MaterialStateProperty.all(
+                                      const TextStyle(fontSize: 15))),
+                              child: const Text('Regresar'),
+                              onPressed: () => Navigator.pop(context),
+                            )
+                          ]),
+                    ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget stackSinPos() {
+    return Center(
+      child: SizedBox(
+        width: 300.0,
+        height: 300.0,
+        child: Center(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                width: 300.0,
+                height: 300.0,
+                color: Colors.red,
+                alignment: Alignment.topRight,
+                padding: const EdgeInsets.all(15.0),
+                child: const Text('Uno', style: TextStyle(color: Colors.white)),
+              ),
+              Container(
+                width: 250.0,
+                height: 250.0,
+                color: Color.fromARGB(255, 54, 244, 187),
+                alignment: Alignment.bottomLeft,
+                padding: const EdgeInsets.all(15.0),
+                child: const Text('Dos', style: TextStyle(color: Colors.white)),
+              ),
+              Container(
+                width: 200.0,
+                height: 200.0,
+                //color: Color.fromARGB(255, 193, 244, 54),
+                alignment: Alignment.bottomCenter,
+                padding: const EdgeInsets.all(15.0),
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://res.cloudinary.com/teepublic/image/private/s--lxNXHPN3--/c_fit,g_north_west,h_840,w_679/co_ffffff,e_outline:40/co_ffffff,e_outline:inner_fill:1/co_ffffff,e_outline:40/co_ffffff,e_outline:inner_fill:1/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1585726530/production/designs/8796655_0.jpg'))),
+                child:
+                    const Text('Tres', style: TextStyle(color: Colors.black)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
